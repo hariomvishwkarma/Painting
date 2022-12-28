19e6d6cb5a2ba7a43c354fdde6957a54b34d88e4
hariomvishwakarma1 <117162788+hariomvishwakarma1@users.noreply.github.com>
2022-12-28 17:24:34 +0530

    painting

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+// $(document).ready(function() {
+//     $('.main-image-banner-slider').slick({
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         arrows: true,
+//         dots: true,
+//         speed: 1800,
+//         infinite: true,
+//         autoplaySpeed: 5000,
+//         autoplay: false,
+//     });
+// });
